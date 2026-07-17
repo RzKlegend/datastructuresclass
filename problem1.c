ca4b330b99d25987619d0d2ee361645c7560c2e2
Akash <akash@example.com>
2026-07-17 15:05:24 +0530

    new problems 17.7.26

--- problem1.c
+++ problem1.c
@@ -1,32 +1,35 @@
 #include<stdio.h>
 int main(){
     int arr[100],n,key,i,found = -1;
+    int count = 0;
 
     printf("enter the number of elements:");
     scanf("%d",&n);
 
-    printf("enter the elements of the array:");
+    printf("enter the roll nos of the array:");
 
     for( i = 0 ; i<n ; i++){
         scanf("%d", &arr[i]);
     }
 
-    printf("enter the element to search:");
+    printf("enter the roll no to search:");
     scanf("%d",&key);
     
     for(i=0;i<n;++i){
+        count++;
         if (arr[i]==key){
             found = i;
             break;
+
         }
     }
 
     if (found != -1){
-        printf("element found at index: %d", found);
+        printf("roll no found at index: %d\n ", found);
         }
     else{
-            printf("element  is not found at all");
+            printf("roll no  is not found at all\n ");
         }
-    
+    printf("\nthe number of comparisions are: %d",count);
     return 0;
 }
